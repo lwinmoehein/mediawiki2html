01e69812db88095894951816561dfa7602b78ce1
lwinmoehein <dev.lwinmoehein@gmail.com>
2022-01-17 12:57:53 +0630

    refactor to function

--- extreactsounds.py
+++ extreactsounds.py
@@ -1,6 +1,26 @@
 import re
 
 
+def extractSoudsSections(section):
+  soundLinesPattern = re.compile(r'\*\s\{{2}.+\}{2}')
+  lineMatches = soundLinesPattern.finditer(section)
+
+  finalSounds = ""
+  for lineMatch in lineMatches:
+    line = string[lineMatch.start():lineMatch.end()]
+    pattern = re.compile(r"\{{2}(\ba\|[\w\s]+\b|\bIPA.+|\benPR.+\b|\brhymes.+)\}{2}")
+
+    matches = pattern.finditer(line)
+
+    finalLine = ""
+    for match in matches:
+      start = match.start()
+      end = match.end()
+      finalLine+= line[start+2:end-2]+","
+    finalSounds+="* "+finalLine+"\n"
+  return finalSounds
+
+
 string = '''
    * {{a|US}}, {{a|UK}} {{enPR|fŭk}}, {{IPA2|/fʌk/}}
 * {{rhymes|ʌk}}
@@ -9,22 +29,4 @@ string = '''
 * {{rhymes|ʊk}}
 '''
 
-soundLinesPattern = re.compile(r'\*\s\{{2}.+\}{2}')
-lineMatches = soundLinesPattern.finditer(string)
-
-finalSounds = ""
-for lineMatch in lineMatches:
-  line = string[lineMatch.start():lineMatch.end()]
-  print("line:"+line)
-  pattern = re.compile(r"\{{2}(\ba\|[\w\s]+\b|\bIPA.+|\benPR.+\b|\brhymes.+)\}{2}")
-
-  matches = pattern.finditer(line)
-
-  finalLine = ""
-  for match in matches:
-    print(match)
-    start = match.start()
-    end = match.end()
-    finalLine+= line[start+2:end-2]+","
-  finalSounds+="* "+finalLine+"\n"
-print(finalSounds)
+print(extractSoudsSections(string))
